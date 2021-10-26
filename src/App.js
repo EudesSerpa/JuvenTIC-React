ab010e96acab0cf8c18c8162554fdedf436803f5
EudesSerpa <serpagame@gmail.com>
2021-10-26 15:58:19 -0500

    Changed about page route

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import './App.css';
 
 // import NavBar from './Components/NavBar';
 import Home from './pages/Home';
-// import About from './pages/About';
-import {About} from './Components/about/about';
+import About from './pages/About';
 import Menu from './pages/Menu';
 import Services from './pages/Services';
 import Reserves from './pages/Reserves';
