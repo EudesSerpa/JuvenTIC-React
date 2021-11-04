671a545538cd6761a160f2a279dee313c7c2b61a
EudesSerpa <serpagame@gmail.com>
2021-11-04 17:30:11 -0500

    Deleted welcome message

--- src/pages/Menu/index.js
+++ src/pages/Menu/index.js
@@ -1,28 +1,7 @@
-import React, {useContext, useEffect} from 'react'
+import React from 'react'
 import NavBar from '../../Components/NavBar/index';
-import AuthContext from '../../Context/autenticacion/authContext';
-import Swal from 'sweetalert2';
 
 export default function Menu() {
-
-    const authContext = useContext(AuthContext);
-    const {autenticado, usuario, usuarioAutenticado} = authContext;
-
-    useEffect(()=>{
-        // Es asincrono? El nombre no aparece en la 1° ejecucion per se
-        usuarioAutenticado();
-
-        if(autenticado){
-            Swal.fire({
-                icon: 'success',
-                title: `Bienvenido ${usuario?.nombre}`,
-                showConfirmButton: false,
-                timer: 2000,
-            });
-        }
-    }, [])
-
-
     return (
         <div>
             <NavBar/>
