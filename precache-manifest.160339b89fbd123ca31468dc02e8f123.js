a9cef3d9e22c96ad150b98b61b8fae27dbaeb6e5
EudesSerpa <serpagame@gmail.com>
2021-11-04 12:53:57 -0500

    Updates

---
+++ precache-manifest.160339b89fbd123ca31468dc02e8f123.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916bcb400c30161f6dfcb35b0aa51b25",
+    "url": "/JuvenTIC-React/index.html"
+  },
+  {
+    "revision": "345f529233d10beb128d",
+    "url": "/JuvenTIC-React/static/css/main.0e9db528.chunk.css"
+  },
+  {
+    "revision": "fa8e0e13cad0834e88ae",
+    "url": "/JuvenTIC-React/static/js/2.76eb785e.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/JuvenTIC-React/static/js/2.76eb785e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "345f529233d10beb128d",
+    "url": "/JuvenTIC-React/static/js/main.9532112e.chunk.js"
+  },
+  {
+    "revision": "1171114da0d3a7938855",
+    "url": "/JuvenTIC-React/static/js/runtime-main.eca9bc02.js"
+  },
+  {
+    "revision": "35e24a76ed2393862f96fae652b65504",
+    "url": "/JuvenTIC-React/static/media/arrowLeft.35e24a76.svg"
+  },
+  {
+    "revision": "5bede857ede77016ad8f921862848d11",
+    "url": "/JuvenTIC-React/static/media/arrowRight.5bede857.svg"
+  },
+  {
+    "revision": "62eefd11215424ec653ab194330af840",
+    "url": "/JuvenTIC-React/static/media/menuClose.62eefd11.svg"
+  },
+  {
+    "revision": "00b27c6bbcdc5e3c924b155ef99e5b23",
+    "url": "/JuvenTIC-React/static/media/menuHamburger.00b27c6b.svg"
+  }
+]);
